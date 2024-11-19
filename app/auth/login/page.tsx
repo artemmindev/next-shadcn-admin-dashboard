fa3aced03d76a8255de6025a87a2b2ef8b0209ff
artemmindev <artemmindev@outlook.com>
2024-11-18 19:00:54 -0500

    add login page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "@/components/app/auth/LoginForm";
+
+
+const page = () => {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default page;
--- components/app/auth/LoginForm.tsx
+++ components/app/auth/LoginForm.tsx
@@ -17,62 +17,33 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-const RegisterForm = () => {
-  const formSchema = z
-    .object({
-      username: z.string().min(1, "Username is required").max(30),
-      email: z.string().min(1, "Email is required").email("Invalid email"),
-      password: z
-        .string()
-        .min(1, "Password is required")
-        .min(8, "Password must have than 8 characters"),
-      confirmPassword: z
-        .string()
-        .min(1, "Password confirmation is required")
-        .min(8, "Password must have than 8 characters"),
-    })
-    .refine((data) => data.password === data.confirmPassword, {
-      path: ["confirmPassword"],
-      message: "Password should be matched",
-    });
+const LoginForm = () => {
+  const formSchema = z.object({
+    email: z.string().min(1, "Email is required").email("Invalid email"),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, "Password must have than 8 characters"),
+  });
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
       email: "",
       password: "",
-      confirmPassword: "",
     },
   });
 
   const onSubmit = () => {
     form.reset();
-    redirect("/auth/login");
+    redirect("/");
   };
 
   return (
     <Form {...form}>
       <div className="w-96">
-        <form onSubmit={form.handleSubmit(onSubmit)}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
           <div className="space-y-2">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="johndoe"
-                      autoComplete="off"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
             <FormField
               control={form.control}
               name="email"
@@ -108,36 +79,18 @@ const RegisterForm = () => {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="confirmPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Confirm password</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Re-Enter your password"
-                      type="password"
-                      autoComplete="off"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
           </div>
           <Button className="w-full mt-6" type="submit">
-            Sign up
+            Sign in
           </Button>
         </form>
         <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
           or
         </div>
         <p className="text-center text-sm text-gray-600 mt-2">
-          If you already have an account, please&nbsp;
-          <Link className="text-blue-500 hover:underline" href="/auth/login">
-            Sign in
+          If you don&apos;t have an account, please&nbsp;
+          <Link className="text-blue-500 hover:underline" href="/auth/register">
+            Sign up
           </Link>
         </p>
       </div>
@@ -145,4 +98,4 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm;
+export default LoginForm;
