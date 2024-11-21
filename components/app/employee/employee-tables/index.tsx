df565158546d71d7642c3a7b44b0bb778eff5e71
artemmindev <artemmindev@outlook.com>
2024-11-21 13:58:42 -0500

    add search funtion into employee page

--- components/app/employee/employee-tables/index.tsx
+++ components/app/employee/employee-tables/index.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import { DataTable } from "@/components/ui/common/table/DataTable";
+import DataTableFilterBox from "@/components/ui/common/table/DataTableFilterBox";
+import DataTableResetFilter from "@/components/ui/common/table/DataTableResetFilter";
+import DataTableSearch from "@/components/ui/common/table/DataTableSearch";
 import { Employee } from "@/constants/data";
 import { columns } from "../employee-tables/Columns";
+import {
+  GENDER_OPTIONS,
+  useEmployeeTableFilters,
+} from "./useEmployeeTableFilters";
 
 export default function EmployeeTable({
   data,
@@ -11,8 +18,37 @@ export default function EmployeeTable({
   data: Employee[];
   totalData: number;
 }) {
+  const {
+    genderFilter,
+    setGenderFilter,
+    isAnyFilterActive,
+    resetFilters,
+    searchQuery,
+    setPage,
+    setSearchQuery,
+  } = useEmployeeTableFilters();
+
   return (
     <div className="space-y-4">
+      <div className="flex flex-wrap items-center gap-4">
+        <DataTableSearch
+          searchKey="name"
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          setPage={setPage}
+        />
+        <DataTableFilterBox
+          filterKey="gender"
+          title="Gender"
+          options={GENDER_OPTIONS}
+          setFilterValue={setGenderFilter}
+          filterValue={genderFilter}
+        />
+        <DataTableResetFilter
+          isFilterActive={isAnyFilterActive}
+          onReset={resetFilters}
+        />
+      </div>
       <DataTable columns={columns} data={data} totalItems={totalData} />
     </div>
   );
